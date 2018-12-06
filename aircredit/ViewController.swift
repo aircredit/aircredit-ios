5d92c8842c7bbff115691f16b87f1853f914a75e
Michel Courtine <github@michak.net>
2018-12-06 13:22:27 +0000

    initial commit

--- aircredit/ViewController.swift
+++ aircredit/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    private var player: AVPlayer!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Load the video from the app bundle.
+        let videoURL: URL? = Bundle.main.url(forResource: "video", withExtension: "mov")
+        if (videoURL != nil){
+            player = AVPlayer(url: videoURL!)
+            player?.actionAtItemEnd = .none
+            player?.isMuted = true
+            
+            let playerLayer = AVPlayerLayer(player: player)
+            playerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            playerLayer.zPosition = -1
+            playerLayer.frame = view.frame
+            view.layer.addSublayer(playerLayer)
+            
+            player?.play()
+            
+            //loop video
+            NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: self.player.currentItem, queue: .main) { [weak self] _ in
+                self?.player?.seek(to: CMTime.zero)
+                self?.player?.play()}
+        }
     }
-
-
 }
-
