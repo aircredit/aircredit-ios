a837309a3ba7ea7b7eae4261e74cd627ec56a9e0
Michel Courtine <github@michak.net>
2018-12-06 12:24:44 +0000

    Initial Commit

---
+++ aircredit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aircredit
+//
+//  Created by Michel Courtine on 12/6/18.
+//  Copyright © 2018 Michel Courtine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
