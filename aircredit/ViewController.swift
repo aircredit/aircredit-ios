9b0dcdadbf01b0034f1ddc630acccd5e5bcfc710
Michel Courtine <github@michak.net>
2018-12-10 16:25:27 +0000

    Revert "added FlowingMenu with dummy placeholders"
    This reverts commit 78072474bb48e2582a4c61969546a6b8af614e64.
    
    Signed-off-by: Michel Courtine <github@michak.net>

--- aircredit/ViewController.swift
+++ aircredit/ViewController.swift
@@ -9,65 +9,14 @@
 import UIKit
 import AVKit
 import AVFoundation
-import FlowingMenu
 
-class ViewController: UIViewController, FlowingMenuDelegate {
-    @IBOutlet var flowingMenuTransitionManager: FlowingMenuTransitionManager!
+class ViewController: UIViewController {
 
-    let PresentSegueName = "PresentMenuSegue"
-    let DismissSegueName = "DismissMenuSegue"
-    let CellName         = "UserContactCell"
-    let mainColor      = UIColor(hex: 0x804C5F)
-
-    
     private var player: AVPlayer!
-    var menu: UIViewController?
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == PresentSegueName {
-            let vc                   = segue.destination
-            vc.transitioningDelegate = flowingMenuTransitionManager
-            
-            flowingMenuTransitionManager.setInteractiveDismissView(vc.view)
-            
-            menu = vc
-        }
-    }
-    @IBAction func unwindToMainViewController(_ sender: UIStoryboardSegue) {
-    }
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        
-        if (flowingMenuTransitionManager != nil){
-            flowingMenuTransitionManager.setInteractivePresentationView(view)
-            flowingMenuTransitionManager.delegate = self
-        }
-        
-        playBackgroundVideo()
-    }
-    
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
-    
-    // MARK: - FlowingMenu Delegate Methods
-    
-    func colorOfElasticShapeInFlowingMenu(_ flowingMenu: FlowingMenuTransitionManager) -> UIColor? {
-        return mainColor
-    }
-    
-    func flowingMenuNeedsPresentMenu(_ flowingMenu: FlowingMenuTransitionManager) {
-        performSegue(withIdentifier: PresentSegueName, sender: self)
-    }
-    
-    func flowingMenuNeedsDismissMenu(_ flowingMenu: FlowingMenuTransitionManager) {
-        menu?.performSegue(withIdentifier: DismissSegueName, sender: self)
-    }
-
-    fileprivate func playBackgroundVideo() {
-        //        flowingMenuTransitionManager.delegate = self
         
         // Load the video from the app bundle.
         let videoURL: URL? = Bundle.main.url(forResource: "video", withExtension: "mov")
@@ -78,7 +27,7 @@ class ViewController: UIViewController, FlowingMenuDelegate {
             
             let playerLayer = AVPlayerLayer(player: player)
             playerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-            playerLayer.zPosition = -1
+            playerLayer.zPosition = 1
             playerLayer.frame = view.frame
             view.layer.addSublayer(playerLayer)
             
